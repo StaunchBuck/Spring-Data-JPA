eb72aa922a8504c75bb8847743af17e369f4d66b
fcadmin <fcadmin@192.168.1.4>
2021-09-02 00:36:11 +0530

    changes

---
+++ JPA-Relationship/src/test/java/com/tech/jpa/relationship/JPARelationship/JpaRelationshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.jpa.relationship.JPARelationship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaRelationshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
